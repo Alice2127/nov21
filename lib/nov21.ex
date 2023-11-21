f590b15cc0ac32cd4055486c9030bd03cd35aefb
Alice2127 <aiokada2128@gmail.com>
2023-11-21 23:16:14 +0900

    initial commit

---
+++ lib/nov21.ex
@@ -0,0 +1,18 @@
+defmodule Nov21 do
+  @moduledoc """
+  Documentation for `Nov21`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Nov21.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nov21_test.exs
@@ -0,0 +1,8 @@
+defmodule Nov21Test do
+  use ExUnit.Case
+  doctest Nov21
+
+  test "greets the world" do
+    assert Nov21.hello() == :world
+  end
+end
